ac9c046786d318d398d998268a0062437b0a0af8
Conor O'Malley <comalley88@gmail.com>
2021-10-24 19:39:12 +0200

    bike shop part 3

--- routes/index.js
+++ routes/index.js
@@ -43,47 +43,34 @@ router.get('/', function(req, res) {
   res.render('index', {dataBike});
 });
 
-var dataCardBike = [
-  {
-    nom: 'BIKO45',
-    url: '/images/bike-1.jpg',
-    prix: 679,
-    quantity: 1
-  },
-  {
-    nom: 'ZOOK7',
-    url: '/images/bike-2.jpg',
-    prix: 799,
-    quantity: 1
-  },
-  {
-    nom: 'LIKO89',
-    url: '/images/bike-3.jpg',
-    prix: 839,
-    quantity: 1
-  },
-  {
-    nom: 'GEW08',
-    url: '/images/bike-4.jpg',
-    prix: 1249,
-    quantity: 1
-  },
-  {
-    nom: 'KIWIT',
-    url: '/images/bike-5.jpg',
-    prix: 899,
-    quantity: 1
-  },
-  {
-    nom: 'NASAY',
-    url: '/images/bike-6.jpg',
-    prix: 1399,
-    quantity: 1
-  }
-]
+var dataCardBike = [];
 
 router.get('/shop', function(req,res) {
   console.log(req.query)
+  console.log(dataCardBike)
+  dataCardBike.push({nom: req.query.nom,
+      url: req.query.url,
+      prix: req.query.prix,
+      quantity: 1})
+ 
+  res.render('shop', {dataCardBike})
+})
+
+
+router.get('/delete-shop', function(req,res) {
+  console.log(req.query)
+  for (var i=0; i<dataCardBike.length; i++) {
+    if (dataCardBike[i].nom === req.query.nom) {
+      dataCardBike.splice(dataCardBike[i], 1)
+    }
+  }
+  res.render('shop', {dataCardBike})
+})
+
+router.post('/update-shop', function(req,res) {
+  console.log(req.body)
+  var newQuantity = req.body.quantity
+  dataCardBike[req.body.position].quantity = newQuantity;
   res.render('shop', {dataCardBike})
 })
 
